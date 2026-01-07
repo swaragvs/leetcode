51a5927d1cd08cd71288e794dd6ff29c61f9689b
Swarag V S <swaragvs09@gmail.com>
2026-01-07 10:28:37 +0530

    sync: Clear Digits

---
+++ solutions/3174-clear-digits/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def clearDigits(self, s):
+
+        stack = [] 
+        for char in s: 
+            if char.isdigit():
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(char)
+        return "".join(stack)
+        """
+        :type s: str
+        :rtype: str
+        """
+        
